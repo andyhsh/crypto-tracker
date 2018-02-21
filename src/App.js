68c0207f7938af7f546d853d2d2679128817f880
DIR\andy.see.hung.ho <andy.see.hung.ho@accenture.com>
2018-02-21 19:20:38 +0800

    Create container component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import { data } from './config/dummyData';
 
 class App extends Component {
+  state = {
+    data: []
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Cryptocurrency Real-time Price</h1>
+        {data.map(currency => (
+          <Card
+            key={currency.ticker.base}
+            name={currency.ticker.base}
+            price={currency.ticker.price}
+            volume={currency.ticker.volume}
+            change={currency.ticker.change}
+          />
+        ))}
       </div>
     );
   }
