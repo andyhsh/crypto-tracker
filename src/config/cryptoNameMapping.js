9d79b9432030b0d3517a0daf3bd8cac839fd59a0
DIR\andy.see.hung.ho <andy.see.hung.ho@accenture.com>
2018-02-21 19:21:29 +0800

    Create data config

---
+++ src/config/cryptoNameMapping.js
@@ -0,0 +1,11 @@
+export const mapping = {
+  BTC: 'Bitcoin',
+  ETH: 'Ether',
+  LTC: 'Litecoin',
+  XMR: 'Monero',
+  XRP: 'Ripple',
+  DOGE: 'Dogecoin',
+  MAID: 'MaidSafeCoin',
+  LSK: 'Lisk',
+  SJCX: 'Storjcoin'
+};
---
+++ src/config/dummyData.js
@@ -0,0 +1,122 @@
+export const data = [
+  {
+    ticker: {
+      base: 'BTC',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'ETH',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'LTC',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '-0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'XMR',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '-0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'XRP',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '-0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'DOGE',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '-0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'DASH',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '-0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'MAID',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '-0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'LSK',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '-0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  },
+  {
+    ticker: {
+      base: 'SJCX',
+      target: 'USD',
+      price: '443.7807865468',
+      volume: '31720.1493969300',
+      change: '-0.3766203596'
+    },
+    timestamp: 1399490941,
+    success: true,
+    error: ''
+  }
+];
